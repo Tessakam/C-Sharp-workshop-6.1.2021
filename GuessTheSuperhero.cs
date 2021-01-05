b1d12084f5749b8d4113c45436ff7b1700921de5
Tessakam <68902165+Tessakam@users.noreply.github.com>
2021-01-05 14:00:51 +0100

    Exercise

---
+++ GuessTheSuperhero.cs
@@ -0,0 +1,73 @@
+// Goal: create a guessing game that ask the player for a random superhero
+// Our game will have two classes: Game and Program
+// The method Play will run the game.
+
+
+using System;
+
+namespace GuessTheSuperhero
+{
+    class Game
+    {
+        
+		// Add two variables, one for the players input and one for the target
+		// Add an array of your superheroes the palyer needs to guess
+		
+
+
+
+		
+		// Our random number will be used to select an element. 
+		Random RandomNumber = new Random();
+
+        public void Play()
+        {
+            
+			// Select a random superhero, this depends on the lenght of your array
+			
+
+			// The player will see the different superheroes
+            Console.Write(" Guess which Superhero I am thinking of... is it ");
+			
+			// Create a for loop for your superheroes 
+			
+
+
+
+
+
+
+            Input = Console.ReadLine();
+			
+			// Compare what the player has typed to the random element from your superhero array by using an if else statement.
+
+
+
+
+
+
+
+
+
+
+
+
+
+			// Add a method to "read" the input when the player presses a key
+            
+        }
+    }
+	
+    class Program
+    {
+        static void Main()
+        {
+            // Make an instance (object) of your class and call your method
+			// Use "dotnet run" in the terminal to play your game!
+			
+			
+			
+
+        }
+    }
+}
